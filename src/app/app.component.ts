53ff7b5fcc6dfb81d9ee8ab24049d550ac4c8049
sdalwani1 <shubhamdalwani@hotmail.com>
2020-05-10 00:12:05 +0530

    initialized and done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  public num1:number;
+  public num2:number;
+  public num3:number[] = [];
+
+  find(){
+    for(let i = this.num1; i<=this.num2; i++){
+      if(i%2 == 0){
+        this.num3.push(i);
+      }
+    }
+  }
 }
